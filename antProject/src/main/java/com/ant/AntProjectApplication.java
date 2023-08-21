1d9c3a236bbab95cd146c07ae6c98b74521029cb
da0293 <dy7133@naver.com>
2023-08-21 10:11:39 +0900

    first commit

---
+++ antProject/src/main/java/com/ant/AntProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntProjectApplication.class, args);
+	}
+
+}
